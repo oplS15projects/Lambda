1af14de650b06e2a97fb58b5b009e6d822c8ec8e
norman2005 <norman2005@users.noreply.github.com>
2015-04-25 17:26:13 -0400

    Update images.rkt

--- Images/images.rkt
+++ Images/images.rkt
@@ -1,1 +1,11 @@
 
+#lang racket
+;Norman Mutunga
+;Brian Carlson
+;Joshua Caravetta
+;OPL Final Project © 2015
+(require images/compile-time
+         (for-syntax images/logos))
+(provide the-logo)
+;the logo print out on REP
+(define the-logo (compiled-bitmap (plt-logo #:height 100)))
