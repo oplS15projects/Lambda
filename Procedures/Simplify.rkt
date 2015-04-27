555ea36562cf7befc56cebd9744a4af3b3f5857d
Joshua Caravetta <joshcaravetta@yahoo.com>
2015-04-26 20:10:09 -0700

    Update Simplify.rkt

--- Procedures/Simplify.rkt
+++ Procedures/Simplify.rkt
@@ -15,13 +15,19 @@
 
 (define (matching x)
   (begin (match x
+           [(list ?n 'x '+ ?s) (list ?n 'x '+ ?s)]
+           [(list ?s '+ ?n 'x) (list ?n 'x '+ ?s)]
+           [(list 'x '+ ?n 'x) (list (+ ?n 1) 'x)]
+           [(list ?n 'x '+ 'x) (list (+ ?n 1) 'x)]
+           [(list 'x '+ 'x) (list 2 'x)]
            [(list 'x ?n '+ 'x) (list (+ ?n 1) 'x)]
            [(list ?n 'x '+ 'x) (list (+ ?n 1) 'x)]
            [(list 'x ?n) (list 'x ?n)]
            [(list ?n 'x) (list ?n 'x)]
-           [(list 'x '* 'x) (list 2 'x)]
-           [(list ?s '* ?n) (list (* ?s ?n))]
            [(list ?s '+ ?n) (list (+ ?s ?n))]
+           [(list 'x '+ ?n '+ 'x) (list 2 'x '+ ?n)]
+           [(list 'x '+ 'x '+ ?n) (list 2 'x '+ ?n)]
+           [(list ?n '+ 'x '+ 'x) (list 2 'x '+ ?n)]
            [(list 'x '+ ?s '+ ?n) (list 'x '+ (+ ?s ?n))]
            [(list ?s '+ 'x '+ ?n) (list 'x '+ (+ ?s ?n))]
            [(list ?s '+ ?n '+ 'x) (list 'x '+ (+ ?s ?n))]
@@ -32,23 +38,72 @@
            [(list ?s '+ 'x ?m '+ ?n) (list ?m 'x '+ (+ ?s ?n))]
            [(list ?s '+ ?n '+ 'x ?m) (list ?m 'x '+ (+ ?s ?n))]
            [(list ?s 'x '+ ?n '+ ?m 'x) (list (+ ?s ?m) 'x '+ ?n)]
-           [(list ?s '+ ?n '+ ?a) (list (+ (+ ?s ?n) ?a))])))
+           [(list ?s '+ ?n '+ ?a) (list (+ (+ ?s ?n) ?a))]
+           [(list ?n 'x '- ?s) (list ?n 'x '- ?s)]
+           [(list ?s '- ?n 'x) (list ?n 'x '- ?s)]
+           [(list ?n 'x '- 'x) (list (- ?n 1) 'x)]
+           [(list 'x '- ?n 'x) (list (- ?n 1) 'x)]
+           [(list 'x '- 'x) (list 0)]
+           [(list 'x ?n '- 'x) (list (- ?n 1) 'x)]
+           [(list ?n 'x '- 'x) (list (- ?n 1) 'x)]
+           [(list 'x ?n) (list 'x ?n)]
+           [(list ?n 'x) (list ?n 'x)]
+           [(list ?s '- ?n) (list (- ?s ?n))]
+           [(list 'x '- ?n '- 'x) (list '- ?n)]
+           [(list 'x '- 'x '- ?n) (list '- ?n)]
+           [(list ?n '- 'x '- 'x) (list '- ?n)]
+           [(list 'x '- ?s '- ?n) (list 'x '- (- ?s ?n))]
+           [(list ?s '- 'x '- ?n) (list 'x '- (- ?s ?n))]
+           [(list ?s '- ?n '- 'x) (list 'x '- (- ?s ?n))]
+           [(list ?m 'x '- ?s '- ?n) (list ?m 'x '- (- ?s ?n))]
+           [(list ?s '+ ?m 'x '+ ?n) (list ?m 'x '- (- ?s ?n))]
+           [(list ?s '+ ?n '+ ?m 'x) (list ?m 'x '- (- ?s ?n))]
+           [(list 'x ?m '+ ?s '+ ?n) (list ?m 'x '- (- ?s ?n))]
+           [(list ?s '+ 'x ?m '+ ?n) (list ?m 'x '- (- ?s ?n))]
+           [(list ?s '+ ?n '+ 'x ?m) (list ?m 'x '- (- ?s ?n))]
+           [(list ?s 'x '- ?n '- ?m 'x) (list (- ?s ?m) 'x '- ?n)]
+           [(list ?s '- ?n '- ?a) (list (- (- ?s ?n) ?a))]
+           [(list ?s '/ ?n) (list (/ ?s ?n))]
+           [(list 'x '* ?n) (list ?n 'x)]
+           [(list ?n '* 'x) (list ?n 'x)]
+           [(list ?s '* ?n) (list (* ?s ?n))]
+           [(list 'x '- ?n '+ ?s) (list 'x '+ (- ?s ?n))]
+           [(list ?n '- 'x '+ ?s) (list (+ ?n ?s) '- 'x)]
+           [(list ?n '- ?s '+ 'x) (list (- ?n ?s) '+ 'x)]
+           [(list 'x '+ ?n '- ?s) (list 'x '+ (- ?n ?s))]
+           [(list ?n '+ 'x '- ?s) (list 'x '+ (- ?n ?s))]
+           [(list ?n '+ ?s '- 'x) (list (+ ?n ?s) '- 'x)]
+           [(list ?m 'x '- ?n '+ ?s) (list ?m 'x '+ (- ?s ?n))]
+           [(list ?n '- ?m 'x '+ ?s) (list (+ ?n ?s) '- ?m 'x)]
+           [(list ?n '- ?s '+ ?m 'x) (list (- ?n ?s) '+ ?m 'x)]
+           [(list ?m 'x '+ ?n '- ?s) (list ?m 'x '+ (- ?n ?s))]
+           [(list ?n '+ ?m 'x '- ?s) (list ?m 'x '+ (- ?n ?s))]
+           [(list ?n '+ ?s '- ?m 'x) (list (+ ?n ?s) '- ?m 'x)]
+           [(list 'x '+ ?n '- 'x) (list ?n)]
+           [(list 'x '+ 'x '- '?n) (list 2 'x '- ?n)]
+           [(list ?s 'x '+ ?n '- 'x) (list (- ?s 1) 'x '+ ?n)]
+           [(list 'x '+ ?n '- ?s 'x) (list (- ?s 1) 'x '+ ?n)]
+           [(list ?n '+ ?s 'x '- 'x) (list (- ?s 1) 'x '+ ?n)]
+           [(list ?n '+ 'x '- ?s 'x) (list (- ?s 1) 'x '+ ?n)])))
 
 (define current-op 'none)
 
 (define (simplify exp)
+  (mlist->mstring (simp (mstring->mlist exp))))
+
+(define (simp exp)
   (define (simp-helper exp prvcar currentexp ops-seen)
     (begin
       (if (null? exp)
           (my-flatten (cons (matching currentexp) exp))
           (if (operator? (car exp))
               (if (eq? '* (car exp))
-                  (simplify (append (append (get-last currentexp) (my-flatten (matching (list prvcar (car exp) (car (cdr exp)))))) (cddr exp)))
+                  (simp (append (append (get-last currentexp) (my-flatten (matching (list prvcar (car exp) (car (cdr exp)))))) (cddr exp)))
                   (if (eq? 3 (+ ops-seen 1))
                       (simp-helper (my-flatten (cons (matching currentexp) exp)) '() '() 0)
                       (simp-helper (cdr exp) (car exp) (append currentexp (list (car exp))) (+ ops-seen 1))))
               (simp-helper (cdr exp) (car exp) (append currentexp (list (car exp))) ops-seen)))))
-  (mlist->mstring (simp-helper (mstring->mlist exp) '() '() 0)))
+  (simp-helper  exp '() '() 0))
 
 (define (get-last lst)
   (define (get-last-helper lst last)
